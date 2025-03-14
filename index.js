410603295eca33a795ac516ef7a4ef20f9b52260
Xavier Welker <fakenmae@gmail.com>
2025-03-14 09:29:48 -0700

    Created encryption and generated a URL with that encryption

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 document.querySelector('form').addEventListener('submit', event =>{
     event.preventDefault();
 
-    const input = document.querySelector('input');
-    console.log(input.value);
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const output = document.querySelector('#link-input');
+    output.value = `${window.location}#${encrypted}`;
+    output.select();
 })
